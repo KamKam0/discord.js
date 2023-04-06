205046cac2f3b4048f0f246d3bc1b7228b4c256a
KamKam0 <86293161+KamKam0@users.noreply.github.com>
2023-04-07 00:15:38 +0200

    Delete api directory

--- api/headers/base.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    Authorization: `Bot TOKEN`,
-    'User-Agent': `DiscordBot (@kamkam1_0/discord.js - https://www.npmjs.com/package/@kamkam1_0/discord.js, ${require("../index").version})`
-}
--- api/headers/contenttypes.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    url: {"Content-Type": "application/x-www-form-urlencoded"},
-    file: {"Content-Type": "multipart/form-data; boundary="},
-    basic: {"Content-Type": "application/json"}
-}
