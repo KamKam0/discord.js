fdaa568108a24a55b9cae6e5c266f98563ec6c38
= <=>
2023-11-09 11:32:51 +0100

    messageDelete data now sent

--- src/websocket/events/MESSAGE_DELETE.js
+++ src/websocket/events/MESSAGE_DELETE.js
@@ -1,14 +1,18 @@
 const Result = require("./results/messagedelete")
 module.exports = async (bot, datas) => {
     const guild = bot.guilds.get(datas.guild_id)
-    var oldmessage_v
-    if(guild) oldmessage_v = guild.messages.get(datas.id)
-    if(oldmessage_v) guild.messages._delete(oldmessage_v.id)
-    
-    if(bot.databaseState){
-        if(!guild) bot.emit(name(), bot, (new Result(datas, bot)))
-        else if(oldmessage_v) bot.emit(name(), bot, oldmessage_v)
+    let dataToSend;
+    if(guild) {
+        dataToSend = guild.messages.get(datas.id)
     }
+
+    if(dataToSend) {
+        guild.messages._delete(dataToSend.id)
+    } else {
+        dataToSend = new Result(datas, bot)
+    }
+    
+    if(bot.databaseState || bot.databaseState === null) bot.emit(name(), bot, dataToSend)
 }
 
 function name(){ return "MESSAGE_DELETE" }
