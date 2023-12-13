df2cabaf398ab517dd4cd37be8499caf361d1fe4
KamKam0 <86293161+KamKam0@users.noreply.github.com>
2023-12-13 22:16:30 +0100

    added event exception and members chunk gateaway events

--- src/websocket/events/GUILD_MEMBERS_CHUNK.js
+++ src/websocket/events/GUILD_MEMBERS_CHUNK.js
@@ -1,3 +1,21 @@
+const Members = require('../../structures/managers/members')
+const Presences = require('../../structures/managers/presences')
+
 module.exports = async (bot, datas) => {
-    return
+    let guild = bot.guilds.get(datas.guild_id)
+    let presences = new Presences(bot, guild.id)
+    presences._addMultiple(datas.presences)
+    let members = new Members(bot, guild.id)
+    members._addMultiple(datas.members)
+    
+    let dataToReturn = {
+        ...datas,
+        guild,
+        presences,
+        members
+    }
+
+    if(bot.databaseState || bot.databaseState === null) bot.emit(name(), bot, dataToReturn)
 }
+
+function name(){ return "GUILD_MEMBERS_CHUNK" }
--- src/websocket/events/GUILD_SCHEDULED_EVENT_EXCEPTIONS_DELETE.js
+++ src/websocket/events/GUILD_SCHEDULED_EVENT_EXCEPTIONS_DELETE.js
@@ -1,5 +1,15 @@
 module.exports = async (bot, datas) => {
-    return
+    const guild = bot.guilds.get(datas.guild_id)
+    
+    if(!datas.guild_id || !guild) return
+
+    let dataToReturn = {
+        guild,
+        guild_id: guild.id,
+        event_id: datas.event_id
+    }
+    
+    if(bot.databaseState || bot.databaseState === null) bot.emit(name(), bot, dataToReturn)
 }
 
 function name(){ return "GUILD_SCHEDULED_EVENT_EXCEPTIONS_DELETE" }
