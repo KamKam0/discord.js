bab2508ea38c6cb1c65525283b5e649470e1ee24
= <=>
2024-03-18 16:03:59 +0100

    bug fix import case

--- src/websocket/events/INTERACTION_CREATE.js
+++ src/websocket/events/INTERACTION_CREATE.js
@@ -1,9 +1,9 @@
 module.exports = async (bot, datas) => {
     let vtype = null
-    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 2) vtype = "button"
-    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 3) vtype = "contextmenu"
-    if(datas.type === 2) vtype = "slash"
-    if(datas.type === 5) vtype = "modal"
+    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 2) vtype = "Button"
+    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 3) vtype = "Contextmenu"
+    if(datas.type === 2) vtype = "Slash"
+    if(datas.type === 5) vtype = "Modal"
     delete datas.guild
     let classResult = require(`../../structures/singles/interactions/${vtype}`)
     bot.emit(name(), bot,  new classResult(datas, bot))
