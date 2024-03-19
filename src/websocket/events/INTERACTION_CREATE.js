ba5b32de98567716d8f79707b02bafbdaa464bde
= <=>
2024-03-19 15:45:43 +0100

    bug fix casing

--- src/websocket/events/INTERACTION_CREATE.js
+++ src/websocket/events/INTERACTION_CREATE.js
@@ -1,7 +1,7 @@
 module.exports = async (bot, datas) => {
     let vtype = null
     if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 2) vtype = "Button"
-    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 3) vtype = "Contextmenu"
+    if(datas.type === 3 && datas.data && datas.data.component_type && datas.data.component_type === 3) vtype = "ContextMenu"
     if(datas.type === 2) vtype = "Slash"
     if(datas.type === 5) vtype = "modal"
     delete datas.guild
